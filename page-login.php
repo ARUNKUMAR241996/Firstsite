24ddf6d88e7bea0c9f1d08a186b74508b1581b8c
Arunkumar g <33564276+ARUNKUMAR241996@users.noreply.github.com>
2019-12-06 12:20:02 +0530

    Add files via upload

---
+++ page-login.php
@@ -0,0 +1,13 @@
+<?php get_header();?>
+<div class="login">
+<form>
+<h1>Login Page</h1>
+<input type="text" class="frm" name="username" placeholder="user name">
+<br>
+<input type="text" class="frm" name="password" placeholder="Password">
+<br>
+<input type="submit" class="lgbtn" name="login" value="Login">
+ <input type="submit" name="reset" class="lgbtn" value="Reset">
+</form>
+</div>
+<?php get_footer();?>
